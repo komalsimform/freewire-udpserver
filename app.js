d71cfaf9aef16abd4b04369480901dfedcf091c4
vikrantsimform <49068433+vikrantsimform@users.noreply.github.com>
2020-03-26 14:21:22 +0530

    updated response

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const server = dgram.createSocket('udp4');
 const express = require("express");
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get('*', function (req, res) {
+    res.sendfile(path.join(__dirname,"index.html"));
+});
 
 app.post("/url/:id", (req, res, next) => {
     console.log('req.body', req.params.id)
@@ -16,7 +21,7 @@ app.post("/url/:id", (req, res, next) => {
     var client = dgram.createSocket('udp4');
     client.on('message', function (msg, info) {
         console.log('Data received from server : ', msg);
-        console.log('Received %d bytes from %s:%d', info);
+        console.log('Client Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
         client.close();
         if (!req.params.id) {
             return res.status(500).json({
@@ -26,7 +31,8 @@ app.post("/url/:id", (req, res, next) => {
         }
         return res.json({
             'success': true,
-            'data': msg.toString()
+            'data': msg.toString(),
+            'info': info.address + ':' + info.port
         });
     });
     client.on('connect', function (port, address) {
@@ -40,20 +46,18 @@ app.post("/url/:id", (req, res, next) => {
         console.log('bytes====', bytes)
     });
 });
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.listen(8084, () => {
+    console.log("Server running on port 8084");
 });
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
     server.close();
 });
-// server.connect(49150, function (port, address) {
-//     console.log('connect port===', port)
-//     console.log('connect address====', address)
-// });
+
 server.on('message', (msg, rinfo) => {
     console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log('Received %d bytes from %s:%d\n',msg.length, rinfo.address, rinfo.port);
     server.send(msg, rinfo.port, 'localhost', function (error) {
         if (error) {
             client.close();
@@ -69,19 +73,4 @@ server.on('listening', () => {
 
     //sending msg
 });
-
-;
-
-
-
-// const server = http.createServer((req, res) => {
-//     if (req.method === 'POST') {
-//         console.log('post request')
-//         // collectRequestData(req, result => {
-//         //     console.log(result);
-//         //     res.end(`Parsed data belonging to ${result.fname}`);
-//         // });
-//     }
-// })
-
 server.bind(49150);
